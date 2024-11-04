a9dcb6b02d7a267e83594902ae5171f047878db1
Nishan Ahmed <md.nishanahmed14@gmail.com>
2024-11-04 09:31:45 +0600

    initial project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import LoginPage from "./pages/auth/LoginPage";
+
+function App() {
+  return (
+    <>
+      <LoginPage />
+    </>
+  );
+}
+
+export default App;
